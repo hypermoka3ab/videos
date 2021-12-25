80530dcd1f2b3a34709800e48c1c66788b2fcb37
mohammedbelgoumri <mohammedbelgoumri@gmail.com>
2021-12-25 05:39:37 +0100

    QDense: animate proof

--- tmp/Analysis/real_numbers.py
+++ tmp/Analysis/real_numbers.py
@@ -514,28 +514,78 @@ class ArchimedeanProperty(Scene):
 
 class QDesnse(Scene):
     def construct(self):
-        real_line = NumberLine([-5, 5], 15) # real line
+        real_line = NumberLine([-4, 4], 15) # real line
         line_label = always_redraw( # R label
             lambda: 
-                MathTex(r"\mathbb{R}").move_to(real_line.number_to_point(-4.6) + UP * 0.5)    
+                MathTex(r"\mathbb{R}").move_to(real_line.number_to_point(-3.6) + UP * 0.5)    
         )
-        x_trcker = ValueTracker(.2) # x tracker
+        x_tracker = ValueTracker(.2) # x tracker
         y_tracker = ValueTracker(.7) # y tracker
         x_label = always_redraw( # x label
-            lambda: MathTex("x", font_size=25).next_to(real_line.number_to_point(x_trcker.get_value()), UP, buff = SMALL_BUFF)
+            lambda: VGroup(
+                MathTex("x", font_size=30).next_to(real_line.number_to_point(x_tracker.get_value()), UP, buff = SMALL_BUFF),
+                Triangle(
+                    stroke_width=0, fill_color=WHITE, fill_opacity=1
+                ).scale(.1).next_to(real_line.number_to_point(x_tracker.get_value()), DOWN, buff=0)
+            )
         )
         y_label = always_redraw( # y label
-            lambda: MathTex("y", font_size=25).next_to(real_line.number_to_point(y_tracker.get_value()), UP, buff = SMALL_BUFF)
+            lambda: VGroup(
+                MathTex("y", font_size=30).next_to(real_line.number_to_point(y_tracker.get_value()), UP, buff = SMALL_BUFF),
+                Triangle(
+                    stroke_width=0, fill_color=WHITE, fill_opacity=1
+                ).scale(.1).next_to(real_line.number_to_point(y_tracker.get_value()), DOWN, buff=0)
+            ) 
         )
-        q = self.get_archimedean(x_trcker.get_value(), y_tracker.get_value())
-        qx_label = MathTex("qx", font_size=25).next_to(real_line.number_to_point(q * x_trcker.get_value()), UP, buff = SMALL_BUFF)
-        qy_label = MathTex("qy", font_size=25).next_to(real_line.number_to_point(q * y_tracker.get_value()), UP, buff = SMALL_BUFF)
-        self.add(real_line, line_label, x_label, y_label, qx_label, qy_label)
+        q = int(np.abs(1 / (y_tracker.get_value() - x_tracker.get_value())))
+        qx_label = always_redraw(
+            lambda: VGroup(
+                MathTex("qx", font_size=30).next_to(real_line.number_to_point(q * x_tracker.get_value()), UP, buff = SMALL_BUFF),
+                Triangle(
+                    stroke_width=0, fill_color=WHITE, fill_opacity=1
+                ).scale(.1).next_to(real_line.number_to_point(x_tracker.get_value() * q), DOWN, buff=0)
+            )
+        )
+        qy_label = always_redraw(
+            lambda: VGroup(
+                MathTex("qy", font_size=30).next_to(real_line.number_to_point(q * y_tracker.get_value()), UP, buff = SMALL_BUFF),
+                Triangle(
+                    stroke_width=0, fill_color=WHITE, fill_opacity=1
+                ).scale(.1).next_to(real_line.number_to_point(y_tracker.get_value() * q), DOWN, buff=0)
+            )
+        )
+        p_label = always_redraw(
+            lambda: MathTex("p", font_size=30).set_color(YELLOW).next_to(real_line.number_to_point(int(q * y_tracker.get_value())), UP, buff = SMALL_BUFF)
+        )
+        p_over_q_label = always_redraw(
+            lambda: VGroup(
+                Triangle(stroke_width=0, fill_color=WHITE, fill_opacity=1).scale(.1).set_color(YELLOW),
+                MathTex(r"p\over q", font_size=30).set_color(YELLOW)
+            ).arrange(DOWN, buff=SMALL_BUFF).next_to(real_line.number_to_point(int(q * y_tracker.get_value()) / q), DOWN, buff=0)
+        )
+
+        self.play(
+            Write(real_line),
+            Write(line_label),
+        )
         self.wait()
-
-    def rational_between_reals(self):
-        pass
-
-    def get_archimedean(self, x, y):
-        return int(np.abs(1 / (y - x)))
-        
+        self.play(
+            Write(x_label[0]),
+            GrowFromCenter(x_label[1]),
+            Write(y_label[0]),
+            GrowFromCenter(y_label[1])
+        )
+        self.wait()
+        self.play(
+            ReplacementTransform(x_label, qx_label),
+            ReplacementTransform(y_label, qy_label)
+        )
+        self.wait()
+        self.play(Write(p_label))
+        self.wait()
+        self.play(
+            ReplacementTransform(p_label, p_over_q_label),
+            ReplacementTransform(qx_label, x_label),
+            ReplacementTransform(qy_label, y_label)
+        )
+        self.wait()
