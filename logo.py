22be56ff65b5da20a68db109d280153cdc7be55f
Mohammed Belgoumri <mohammedbelgoumri@gmail.com>
2023-10-21 13:45:47 +0100

    fixed imports

--- logo.py
+++ logo.py
@@ -1,19 +1,16 @@
 from itertools import product
-from telnetlib import DO
+
 from manim import *
+
 
 class Logo(Scene):
     def construct(self):
         logo = self.logo()
-        logo.rotate(-PI/2)
+        logo.rotate(-PI / 2)
         self.wait()
-        self.play(
-            FadeIn(logo),
-            logo.animate.rotate(PI/2),
-            run_time=2
-        )
+        self.play(FadeIn(logo), logo.animate.rotate(PI / 2), run_time=2)
         self.wait()
-        
+
         # point = Dot(ORIGIN)
         # segment = VGroup(
         #     Dot(RIGHT),
@@ -35,43 +32,38 @@ class Logo(Scene):
     def adjascency_matrix(self, n=4):
         q = np.zeros((1, 1))
         for k in range(n):
-            i = np.identity(2 ** k)
-            q = np.hstack((
-                np.vstack((q, i)),
-                np.vstack((i, q)),
-            ))
+            i = np.identity(2**k)
+            q = np.hstack(
+                (
+                    np.vstack((q, i)),
+                    np.vstack((i, q)),
+                )
+            )
         return q
 
     def logo(self):
         matrix = self.adjascency_matrix()
-
-        order = [
-            0, 1, 3, 7,
-            15, 14, 12, 8,
-            9, 2, 5, 11,
-            6, 13, 10, 4
-        ]
-
+        order = [0, 1, 3, 7, 15, 14, 12, 8, 9, 2, 5, 11, 6, 13, 10, 4]
         dots = VGroup(
             *[Dot(ORIGIN) for _ in range(16)],
         )
-
         for i in range(16):
             r = 3 if i < 8 else 1
-            dots[i] =  Dot(
-                complex_to_R3(r*np.exp(TAU*1j*i/8)), fill_opacity=1,
-                fill_color = "#008b8b" if is_odd(order[i]) else "#000"
+            dots[i] = Dot(
+                complex_to_R3(r * np.exp(TAU * 1j * i / 8)),
+                fill_opacity=1,
+                fill_color="#008b8b" if is_odd(order[i]) else "#000",
             )
-
         lines = VGroup(
             *[
-                Line(dots[i].get_center(), dots[j].get_center()) 
-                for i, j in product(range(16), range(16)) 
+                Line(dots[i].get_center(), dots[j].get_center())
+                for i, j in product(range(16), range(16))
                 if matrix[order[i], order[j]]
             ]
         ).set_z_index(-1)
-        
+
         return VGroup(lines, dots)
+
 
 def is_odd(n):
     parity = 0
@@ -79,4 +71,3 @@ def is_odd(n):
         parity = ~parity
         n = n & (n - 1)
     return parity == 0
-
