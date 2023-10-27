db661584b9803b0c5339dc073187a7880c69d50d
Mohammed Belgoumri <mohammedbelgoumri@gmail.com>
2023-10-27 10:16:04 +0100

    Logo theme

--- zs.py
+++ zs.py
@@ -1,12 +1,15 @@
 from manim import *
+from themes.dracula import dracula
+
+dracula.set()
 
 config.pixel_height = 600
 config.pixel_width = 600
 config.frame_height = 2
 config.frame_width = 2
-config.background_color = "#000"
+
 
 class ZS(Scene):
     def construct(self):
-        zs = MathTex(r"0^\sharp", font_size=100)
+        zs = MathTex(r"0^\sharp", font_size=150)
         self.add(zs)
