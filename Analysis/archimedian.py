f765d2a8a34a87d2f1f3eee2052f84ceef893163
Mohammed Belgoumri <mohammedbelgoumri@gmail.com>
2024-05-12 13:06:46 +1000

    Add theme to analyisis

--- Analysis/archimedian.py
+++ Analysis/archimedian.py
@@ -1,26 +1,37 @@
 from manim import *
 
+from themes.dracula import dracula
+
+dracula.set()
+
+
 class ArchimedianProperty(Scene):
     def construct(self):
         self.illustrate_property()
         self.wait()
         self.state_theorem()
-        self.wait()        
+        self.wait()
         self.prove_it()
 
     def state_theorem(self):
         from theorems import Theorem
-        
+
         theorem = Theorem(
             title="Propriété d'Archimède",
             body=Tex(
-                "Pour tout ", r"\(x>0\)", " et pour tout ",
-                r"\(y\in\mathbb{R}\)", " il existe un ", r"\(n\in\mathbb{N}\)",
-                " tel que ", r"\(nx>y\)", " i.e:",
+                "Pour tout ",
+                r"\(x>0\)",
+                " et pour tout ",
+                r"\(y\in\mathbb{R}\)",
+                " il existe un ",
+                r"\(n\in\mathbb{N}\)",
+                " tel que ",
+                r"\(nx>y\)",
+                " i.e:",
                 r"\[\forall x>0, \forall y\in\mathbb{R}, \exists n\in\mathbb{N}, \ nx>y\]",
                 tex_environment=None,
-                font_size=40
-            )
+                font_size=40,
+            ),
         )
 
         self.play(Write(theorem.title))
@@ -29,15 +40,24 @@ class ArchimedianProperty(Scene):
         self.wait()
         self.play(*[FadeOut(mob) for mob in self.mobjects])
 
-
     def prove_it(self):
-        anti_hyothesis = MathTex(r"\forall n\in\mathbb{N},\ nx \le y").shift(UP).to_edge(LEFT)
-        a_major = MathTex(
-            r"\Rightarrow", r"A=\{nx|n\in\mathbb{N}\} \text{ et majoré}", r", s=\sup{A}"
-        ).next_to(anti_hyothesis, DOWN).to_edge(LEFT)
-        caracterise = MathTex(
-            "s-x < nx", r"\Rightarrow s < (n+1)x", r"\in A", r"\ \square"
-        ).next_to(a_major, DOWN).to_edge(LEFT)
+        anti_hyothesis = (
+            MathTex(r"\forall n\in\mathbb{N},\ nx \le y").shift(UP).to_edge(LEFT)
+        )
+        a_major = (
+            MathTex(
+                r"\Rightarrow",
+                r"A=\{nx|n\in\mathbb{N}\} \text{ et majoré}",
+                r", s=\sup{A}",
+            )
+            .next_to(anti_hyothesis, DOWN)
+            .to_edge(LEFT)
+        )
+        caracterise = (
+            MathTex("s-x < nx", r"\Rightarrow s < (n+1)x", r"\in A", r"\ \square")
+            .next_to(a_major, DOWN)
+            .to_edge(LEFT)
+        )
 
         self.play(Write(anti_hyothesis))
         self.wait()
@@ -51,16 +71,17 @@ class ArchimedianProperty(Scene):
 
     def illustrate_property(self):
         from math import sqrt
-        
-        y_to_x_ratio = 2*sqrt(5)
+
+        y_to_x_ratio = 2 * sqrt(5)
         nxs = ValueTracker(1)
         x_length = ValueTracker(1)
         x_segment = always_redraw(
-            lambda:
-            NumberLine(
+            lambda: NumberLine(
                 [0, nxs.get_value()],
                 unit_size=x_length.get_value(),
-            ).to_edge(LEFT).shift(UP)
+            )
+            .to_edge(LEFT)
+            .shift(UP)
         )
         x_label = MathTex("x").next_to(x_segment, UP)
 
@@ -68,22 +89,17 @@ class ArchimedianProperty(Scene):
         y_segment = NumberLine([0, 1], y_length).to_edge(LEFT)
         y_label = MathTex("y").next_to(y_segment, DOWN)
 
-        self.play(
-            Create(x_segment), Create(y_segment),
-            Write(x_label), Write(y_label)
-        )
-        
+        self.play(Create(x_segment), Create(y_segment), Write(x_label), Write(y_label))
 
         self.wait()
-        for _ in range(1, int(y_to_x_ratio)+1):
+        for _ in range(1, int(y_to_x_ratio) + 1):
             self.play(nxs.animate.increment_value(1))
             self.wait()
 
         self.play(
-            nxs.animate.set_value(int(y_to_x_ratio*1.5)+1),
-            x_length.animate.set_value(x_length.get_value()/1.5)
+            nxs.animate.set_value(int(y_to_x_ratio * 1.5) + 1),
+            x_length.animate.set_value(x_length.get_value() / 1.5),
         )
         self.wait()
 
         self.play(*[FadeOut(mob) for mob in self.mobjects])
-        
